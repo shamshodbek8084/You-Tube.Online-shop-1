9f4bdef4740475d7eeb88b91cd066fc50c05f3fa
shamshodbek8084 <erkinmulkamonov@gmail.com>
2024-10-13 13:18:28 +0500

    Delete Items

--- puddle/dashboard/views.py
+++ puddle/dashboard/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 from item.models import Item
 # Create your views here.
@@ -13,3 +13,6 @@ def index(request):
     return render(request, 'dashboard/index.html',{
         'items':items
     })
+
+
+
